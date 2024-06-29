d564dc5fe8c5f8696db1f5f60c6363654dc5d9c7
amineakhrib <amine.akhrib@europcar.com>
2024-06-29 23:46:30 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/july-date-program/', // Assurez-vous que ce chemin correspond à votre projet
+  build: {
+    outDir: 'dist', // Le dossier où les fichiers seront générés
+  },
+});
